19942e2470d590e9ca30f48425bc38a2cef4b456
Skitovich Ruslan <zloycvetok@gmail.com>
2021-01-07 17:08:46 +0300

    Correction code,methods according to comments

---
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -0,0 +1,43 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import lombok.Value;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String getValidCity() {
+        Random random = new Random();
+        String[] validCity = {"Москва", "Краснодар", "Санкт-Петербург", "Казань", "Севастополь"};
+        return validCity[random.nextInt(validCity.length - 1)];
+    }
+    public static AuthInfo generateAuthInfo(String locale) {
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().firstName().replace("ё","е") + " " + faker.name().lastName().replace("ё", "е");
+        return new AuthInfo(getValidCity(),faker.phoneNumber().phoneNumber(),name);
+    }
+
+    public static String generateDate (int shiftFromCurrentDay) {
+        LocalDate dayDelivery = LocalDate.now().plusDays(shiftFromCurrentDay);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return  dayDelivery.format(formatter);
+    }
+
+
+    @Value
+    public static class AuthInfo {
+        String city;
+        String telephone;
+        String name;
+    }
+}
+
+
+
--- src/test/java/ru/netology/test/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/test/AppCardDeliveryTest.java
@@ -5,48 +5,42 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
+import ru.netology.data.DataGenerator;
 
 import static com.codeborne.selenide.Selenide.element;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.data.DataClass.getFakerInfo;
+import static ru.netology.data.DataGenerator.generateAuthInfo;
+import static ru.netology.data.DataGenerator.generateDate;
 
 public class AppCardDeliveryTest {
 
 
+
+
     @BeforeAll
     static void setUp() {
-
-        Configuration.startMaximized = true;
         Configuration.browser = "firefox";
+        Configuration.startMaximized = true;
+
     }
 
     @Test
     public void shouldRegistered() {
-
-        LocalDate dayDeliveryCard = LocalDate.now().plusDays(3);
-        LocalDate dayDeliveryCardPlusSevenDays = LocalDate.now().plusDays(7);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-
+        DataGenerator.AuthInfo authInfo = generateAuthInfo("ru");
+        int daysAfterToday = 3;
 
         open("http://localhost:9999/");
-        element("[data-test-id='city'] .input__control").setValue(getFakerInfo("ru").getCity());
+        element("[data-test-id='city'] .input__control").setValue(authInfo.getCity());
         element("[data-test-id=date] [placeholder=\"Дата встречи\"]").
-                sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, dayDeliveryCard.format(formatter));
-        element("[data-test-id='name'] .input__control").setValue(getFakerInfo("ru").getName() + " " + getFakerInfo("ru").getSurName());
-        element("[data-test-id='phone'] .input__control").setValue(getFakerInfo("ru").getTelephone());
+                sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, generateDate(daysAfterToday));
+        element("[data-test-id='name'] .input__control").setValue(authInfo.getName());
+        element("[data-test-id='phone'] .input__control").setValue(authInfo.getTelephone());
         element(".checkbox__box").click();
         element(".button__text").click();
-        element(".notification__content").
-                waitUntil(Condition.visible, 10000).shouldHave(Condition.text(dayDeliveryCard.format(formatter)));
-        element("[data-test-id=date] [placeholder=\"Дата встречи\"]").
-                sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, dayDeliveryCardPlusSevenDays.format(formatter));
+        element("[data-test-id=date] [placeholder=\"Дата встречи\"]").shouldHave(Condition.visible);
         element(".button__text").click();
         element("button.button_size_s .button__text").click();
-        element(".notification__content").waitUntil(Condition.visible, 10000).
-                shouldHave(Condition.text(dayDeliveryCardPlusSevenDays.format(formatter)));
+        element(".notification__content").shouldHave(Condition.visible);
     }
 
 
