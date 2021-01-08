2788fb9cfe170b1b49eb4d38b8db228424c2af02
Skitovich Ruslan <zloycvetok@gmail.com>
2021-01-08 11:41:00 +0300

    Correction code,methods according to comments

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -21,7 +21,8 @@ public class DataGenerator {
 
     public static AuthInfo generateAuthInfo(String locale) {
         Faker faker = new Faker(new Locale(locale));
-        String name = faker.name().firstName().replace("ё", "е") + " " + faker.name().lastName().replace("ё", "е");
+        String name = faker.name().firstName().
+                replace("ё", "е") + " " + faker.name().lastName().replace("ё", "е");
         return new AuthInfo(getValidCity(), faker.phoneNumber().phoneNumber(), name);
     }
 
