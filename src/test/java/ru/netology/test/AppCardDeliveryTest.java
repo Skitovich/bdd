a905b281742d6dddfdbd0c78d4eaf654e4697c6b
Skitovich Ruslan <zloycvetok@gmail.com>
2021-01-10 21:01:19 +0300

    Correction code, add check date

--- src/test/java/ru/netology/test/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/test/AppCardDeliveryTest.java
@@ -25,7 +25,9 @@ public class AppCardDeliveryTest {
     @Test
     public void shouldRegistered() {
         DataGenerator.AuthInfo authInfo = generateAuthInfo("ru");
+
         int daysAfterToday = 3;
+        int daysAfterTodayForTest = 5;
 
         open("http://localhost:9999/");
         element("[data-test-id='city'] .input__control").setValue(authInfo.getCity());
@@ -35,10 +37,13 @@ public class AppCardDeliveryTest {
         element("[data-test-id='phone'] .input__control").setValue(authInfo.getTelephone());
         element(".checkbox__box").click();
         element(".button__text").click();
-        element("[data-test-id=date] [placeholder=\"Дата встречи\"]").shouldHave(Condition.visible);
+        element(".notification__content").shouldHave(Condition.visible).
+                waitUntil(Condition.text(generateDate(daysAfterToday)), 10000);
         element(".button__text").click();
-        element("button.button_size_s .button__text").click();
-        element(".notification__content").shouldHave(Condition.visible);
+        element("[data-test-id=date] [placeholder=\"Дата встречи\"]").
+                sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, generateDate(daysAfterTodayForTest));
+        element(".notification__content").shouldHave(Condition.visible).
+                waitUntil(Condition.text(generateDate(daysAfterTodayForTest)), 10000);
     }
 
 
